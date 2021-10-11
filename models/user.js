30a3f726f45345f27df671bd732db85fafbb9861
Yury Avdeev <avdeiev@gmail.com>
2021-10-11 10:53:00 +0300

    signup_email_is_unique

--- models/user.js
+++ models/user.js
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     validate: {
       validator: (email) => validator.isEmail(email),
       message: 'Неправильный формат почты',
