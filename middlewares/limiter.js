4f437309db10086a8f418c2dd454dc3c47c682f2
Yury Avdeev <avdeiev@gmail.com>
2021-11-25 19:53:13 +0300

    limiter_100

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,5 +2,5 @@ const rateLimit = require('express-rate-limit');
 
 module.exports.limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // за 15 минут
-  max: 250, // можно совершить максимум 100 запросов с одного IP
+  max: 100, // можно совершить максимум 100 запросов с одного IP
 });
